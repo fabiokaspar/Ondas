4fe5496709bdb00526fa43f77e17b758a253ff3c
fabiokaspar <fabiokaspar@gmail.com>
2016-01-02 14:10:09 -0200

    Desenha a mesma circunferência, mas agora no bitmap e não no display. [OK]

--- main.c
+++ main.c
@@ -1,22 +1,22 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
  
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
-
-#define FRACAO_FAIXA    0.70
  
 bool inicializar();
 void varreFaixa(int linIn, int linF, int colIn, int colF);
 void varreCirculo(float cx, float cy, float r);
  
-const int LARGURA_TELA = 600;
-const int ALTURA_TELA = 600;
- 
+
+#define FRACAO_FAIXA    0.70
+
+const int LARGURA_TELA = 1024;
+const int ALTURA_TELA = 768;
 ALLEGRO_DISPLAY *janela = NULL;
 ALLEGRO_BITMAP *bmp = NULL;
-FILE* arq;
 
 int main(int argc, char** argv) {
     float cx, cy, r;
@@ -34,33 +34,18 @@ int main(int argc, char** argv) {
     int h = al_get_bitmap_height(bmp);
     int w = al_get_bitmap_width(bmp);
     printf("height = %d\n", h);
-    printf("width = %d\n", w);
+    printf("width = %d\n\n", w);
         
     al_set_target_bitmap(bmp);
     al_clear_to_color(al_map_rgb(255,255,255));
-    al_draw_circle(cx, cy, r, al_map_rgb(250, 0, 0), 2);
+    al_draw_circle(cx, cy, r, al_map_rgb(250, 0, 0), 1.0);
     
-    al_set_target_backbuffer(janela);
-    al_draw_bitmap(bmp, 0, 0, 0);    
-    al_flip_display();
-    al_rest(3);
-
-    bmp = al_get_backbuffer(janela);    
-    al_set_target_bitmap(bmp);
-    arq = fopen("out.txt", "w");
-
     varreCirculo(cx, cy, r);
 
-    fclose(arq);
-    
-    al_set_target_backbuffer(janela);
-    al_draw_bitmap(bmp, 0, 0, 0);    
-    al_flip_display();
-    while (1);
-    //al_rest(3.0);    
-    
-    //al_destroy_display(janela);
-    
+    if(!al_save_bitmap("ondas.pcx", bmp))
+        printf("Não salvou!\n"); 
+
+    printf("TERMINOU.\n");
     return 0;
 }
 
@@ -90,13 +75,8 @@ void varreFaixa(int linIn, int linF, int colIn, int colF) {
             if (R == 250) {
                 cor = al_map_rgb(0, 0, 0);
                 al_put_pixel(x, y, cor);
-                fprintf(arq,"x");
-            }
-            else {
-                fprintf(arq," ");
             }
         }
-        fprintf(arq,"\n");
     }
 }
  
@@ -113,15 +93,12 @@ bool inicializar()
         fprintf(stderr, "Falha ao inicializar add-on de primitivas.\n");
         return false;
     }
- 
-    janela = al_create_display(LARGURA_TELA, ALTURA_TELA);
-    if (!janela)
+
+    if (!al_init_image_addon())
     {
-        fprintf(stderr, "Falha ao criar janela.\n");
+        fprintf(stderr, "Falha ao inicializar image_addon().\n");
         return false;
     }
- 
-    al_set_window_title(janela, "Testando allegro_primitives");
- 
+    
     return true;
 }
